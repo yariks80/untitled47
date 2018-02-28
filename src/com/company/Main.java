e654fe9de4a55e90f0a4bb4f164545bd0c26a110
yarik <yariks@i.ua>
2018-02-28 10:27:52 +0200

    commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+
     }
 }
