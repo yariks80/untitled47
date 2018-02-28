f28b90fa500f9122168dc1320213e9c431725bbc
yarik <yariks@i.ua>
2018-02-28 10:30:09 +0200

    commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,10 @@ public class Main {
         System.out.println("Hi");
         System.out.println("Hi");
         System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
 
     }
 }
